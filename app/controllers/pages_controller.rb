411101d173d3184fbdebfb046ce624e7387830ad
nelcifran <nelcifranpires@gmail.com>
2018-12-08 11:52:10 -0300

    changes in artist index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	
   def home
     @band = Band.first
-    @artists = Artist.order(created_at: :asc).all
+    @artists = Artist.order(created_at: :desc).all
     @tours = Tour.order(date_show: :desc).all
     @discographies = Discography.all
   end
